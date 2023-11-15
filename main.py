a22a2dce9bf38609be73a5a6fb6b2bf753bfb86a
Erik Vasilyan <110052720+erikvasilyan@users.noreply.github.com>
2023-11-15 16:46:43 +0100

    Actualització main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+def play_music():
+    music.play(music.builtin_playable_sound_effect(soundExpression.giggle), 
+    music.PlaybackMode.UNTIL_DONE)
 
+def play_my_music():
+    music.play(music.string_playable("G F G A - F E D", 120),
+    music.PlaybackMode.UNTIL_DONE)
+
+def speed():
+    x = input.acceleration(Dimension.X)
+    led.plot_bar_graph(x, 1023)
+    if x > 300:
+        music.change_tempo_by(20)
+    else:
+        music.change_tempo_by(-20)
+
+input.on_button_pressed(Button.A, play_music)
+input.on_button_pressed(Button.B, play_my_music)
+basic.forever(speed)
--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+input.onButtonPressed(Button.A, function play_music() {
+    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function play_my_music() {
+    music.play(music.stringPlayable("G F G A - F E D", 120), music.PlaybackMode.UntilDone)
+})
+basic.forever(function speed() {
+    let x = input.acceleration(Dimension.X)
+    led.plotBarGraph(x, 1023)
+    if (x > 300) {
+        music.changeTempoBy(20)
+    } else {
+        music.changeTempoBy(-20)
+    }
+    
+})
